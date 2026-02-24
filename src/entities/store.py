0aff0c9c9e0c1ef5eb9f83f7f932cf06c8e227ba
gabrielms-castro <gabrielms.castro@gmail.com>
2026-02-24 16:59:30 -0300

    refact: inserido access token dentro da classe Store. adicionado metodos get_access_token() e set_access_token()

--- src/entities/store.py
+++ src/entities/store.py
@@ -14,6 +14,13 @@ class Store:
         self.pv = pv
         self.token = token
         self.b64_credential = encode_credentials(self.pv, self.token)
+        self.access_token = None
+
+    def get_access_token(self) -> str:
+        return self.access_token
+    
+    def set_access_token(self, access_token) -> None:
+        self.access_token = access_token
 
         
     
