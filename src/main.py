4b3c038fc799507d7ac4d110088278d40911cf3c
gabrielms-castro <gabrielms.castro@gmail.com>
2026-02-24 17:55:35 -0300

    feat: refatorado o método main para incluir criação de transação e impressão de detalhes

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+import random
+
+from src.dataclasses.http_methods import HttpMethods
+from src.services.transaction_service import TransactionService
 from src.config import PV, TOKEN
 from src.entities.environment import Environment
 from src.entities.store import Store
@@ -6,11 +10,51 @@ from src.entities.erede import eRede
 
 def main():
     store = Store(PV, TOKEN, Environment.sandbox())
+    
+    erede = eRede(store)
+    
+    access_token = erede.get_access_token()
 
-    access_token = eRede(store).get_access_token()
-    print(access_token)
+    store.set_access_token(access_token)
+    
+    reference = "g" + str(random.randint(10**(16 - 1), (10**16)-1))
+    
+    json = {
+        "capture": False,
+        "kind": "credit",
+        "reference": reference,
+        "amount": 2000,
+        "installments": 2,
+        "cardholderName": "John Snow",
+        "cardNumber": "5448280000000007",
+        "expirationMonth": 1,
+        "expirationYear": 2028,
+        "securityCode": "123",
+        "softDescriptor": "string",
+        "subscription": False,
+        "origin": 1,
+        "distributorAffiliation": 0,
+        "brandTid": "string",
+        "storageCard": "0",
+        "transactionCredentials": {
+            "credentialId": "01"
+        }
+    }
 
 
+    transaction = erede.create(json)
+    print(transaction)
+
+    print()
+    tid = transaction.get("tid")
+    print(tid)
+
+    print()
+    print(erede.get_by_tid(tid))
+
+    print()
+    print(erede.get_by_reference(reference))
+    
 
 if __name__ == "__main__":
     main()
