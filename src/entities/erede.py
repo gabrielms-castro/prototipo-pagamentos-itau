eefc8e35427cb4e01181760391474102a7cc9bd5
gabrielms-castro <gabrielms.castro@gmail.com>
2026-02-24 17:00:10 -0300

    feat: criado metodos get_by_tid, get_by_reference e get_refunds

--- src/entities/erede.py
+++ src/entities/erede.py
@@ -1,3 +1,4 @@
+from src.services.get_transaction_service import GetTransactionService
 from src.dataclasses.http_methods import HttpMethods
 from src.entities.store import Store
 from src.services.authentication_service import AuthenticationService
@@ -9,4 +10,29 @@ class eRede:
 
     def get_access_token(self):
         auth_service = AuthenticationService(self.store)
-        return auth_service.send_request(HttpMethods.POST, self.store.b64_credential)
+        response = auth_service.send_request(HttpMethods.POST, self.store.b64_credential)
+        return response.get("access_token")
+
+    def get_by_tid(self, tid):
+        """Get a Transaction by its TID
+        """
+
+        get_transaction = GetTransactionService(self.store)
+        get_transaction.tid = tid
+        return get_transaction.execute()
+    
+    def get_by_reference(self, reference):
+        """Get a Transaction by its reference
+        """
+
+        get_transaction = GetTransactionService(self.store)
+        get_transaction.reference = reference
+        return get_transaction.execute()    
+    
+    def get_refunds(self, tid):
+        """Get a Transaction refunds
+        """
+        get_transaction = GetTransactionService(self.store)
+        get_transaction.tid = tid
+        get_transaction.refunds = True
+        return get_transaction.execute()
