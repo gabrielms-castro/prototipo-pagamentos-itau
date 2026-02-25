7624223f39a0db7389aaff29e29f19593b2399bd
gabrielms-castro <gabrielms.castro@gmail.com>
2026-02-25 17:57:06 -0300

    update: alterado main() com novas formas de consumir a aplicação

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import random
 
 from src.dataclasses.http_methods import HttpMethods
-from src.services.transaction_service import TransactionService
+from src.entities.card import Card
+from src.entities.transaction import Transaction
+from src.enums.card import CardTypes
 from src.config import PV, TOKEN
 from src.entities.environment import Environment
 from src.entities.store import Store
 from src.entities.erede import eRede
+from src.utils import convert
 
 
 def main():
@@ -14,47 +17,36 @@ def main():
     erede = eRede(store)
     
     access_token = erede.get_access_token()
-
     store.set_access_token(access_token)
+
     
     reference = "g" + str(random.randint(10**(16 - 1), (10**16)-1))
-    
-    json = {
-        "capture": False,
-        "kind": "credit",
-        "reference": reference,
-        "amount": 2000,
-        "installments": 2,
-        "cardholderName": "John Snow",
-        "cardNumber": "5448280000000007",
-        "expirationMonth": 1,
-        "expirationYear": 2028,
-        "securityCode": "123",
-        "softDescriptor": "string",
-        "subscription": False,
-        "origin": 1,
-        "distributorAffiliation": 0,
-        "brandTid": "string",
-        "storageCard": "0",
-        "transactionCredentials": {
-            "credentialId": "01"
-        }
-    }
+    amount = convert(2000.00)
+
+    transaction = Transaction(amount, reference)
+    transaction.credit_card(
+        card_number="5448280000000007",
+        security_code="123",
+        expiration_month=1,
+        expiration_year=2028,
+        card_holder_name="John Snow",
+    ).capture_transaction(False)
 
 
-    transaction = erede.create(json)
-    print(transaction)
-
-    print()
-    tid = transaction.get("tid")
-    print(tid)
-
-    print()
-    print(erede.get_by_tid(tid))
-
-    print()
-    print(erede.get_by_reference(reference))
-    
+    transaction_response = erede.create(transaction)
+    print(transaction_response.serialize())
+    if transaction_response.returnCode == "00":
+        print("Transação criada com sucesso!")
+        query_transaction = erede.get_by_tid(transaction_response.tid)
+        print(f"\nTransação consultada: {query_transaction.serialize()}")
+        
+        erede.capture(transaction_response)
+        query_transaction = erede.get_by_tid(transaction_response.tid)
+        print(f"\nTransação consultada: {query_transaction.serialize()}")
+        
+        erede.cancel(transaction_response)
+        query_transaction = erede.get_by_tid(transaction_response.tid)
+        print(f"\nTransação consultada: {query_transaction.serialize()}")
 
 if __name__ == "__main__":
     main()
