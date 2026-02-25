fae7aefae21d3a13a134202ee55516dd8bc31177
gabrielms-castro <gabrielms.castro@gmail.com>
2026-02-25 17:54:24 -0300

    feat: CreateTransactionService agora recebe um objeto Transaction e envia os dados serializados desse objeto na requisição

--- src/services/create_transaction_service.py
+++ src/services/create_transaction_service.py
@@ -1,12 +1,13 @@
 from src.entities.store import Store
 from src.dataclasses.http_methods import HttpMethods
+from src.entities.transaction import Transaction
 from src.services.transaction_service import TransactionService
 
 
 class CreateTransactionService(TransactionService):
-    def __init__(self, store: Store):
+    def __init__(self, store: Store, transaction: Transaction):
         super().__init__(store)
-        self.body = None
+        self.transaction = transaction
 
-    def execute(self):
-        return self.send_request(HttpMethods.POST, self.body)
+    def execute(self) -> Transaction:
+        return self.send_request(HttpMethods.POST, self.transaction.to_json())
