e377877f28029c7b32e9b4dff029244f9ac4d7cb
gabrielms-castro <gabrielms.castro@gmail.com>
2026-02-26 15:25:05 -0300

    feat: update to_json method to use RecursiveEncoder for serialization

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -3,8 +3,23 @@ import json
 class BaseModel:
 
     def to_json(self):
-        return json.dumps(self.serialize())
+        return json.dumps(self.serialize(), cls=RecursiveEncoder)
 
     def serialize(self):
-        return { key: value for key, value in self.__dict__.items() if value is not None }
+        collection = { key: value for key, value in self.__dict__.items() if value is not None }
 
+        for key, value in collection.items():
+            if hasattr(value, "serialize"):
+                collection[key] = value.serialize()
+                
+        return collection
+
+
+class RecursiveEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+
+        if hasattr(obj, "serialize"):
+            return obj.serialize()
+        
+        return super().default(obj)
